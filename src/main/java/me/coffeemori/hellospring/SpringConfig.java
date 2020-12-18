453ec130f2dd542212ff7c74475368cd3cf951b5
coffeemori92 <coffeemori92@naver.com>
2020-12-18 19:51:14 +0900

    순수jdbc

--- src/main/java/me/coffeemori/hellospring/SpringConfig.java
+++ src/main/java/me/coffeemori/hellospring/SpringConfig.java
@@ -1,8 +1,12 @@
 package me.coffeemori.hellospring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import me.coffeemori.hellospring.repositories.JdbcMemberRepository;
 import me.coffeemori.hellospring.repositories.MemberRepository;
 import me.coffeemori.hellospring.repositories.MemoryMemberRepository;
 import me.coffeemori.hellospring.services.MemberService;
@@ -10,6 +14,13 @@ import me.coffeemori.hellospring.services.MemberService;
 @Configuration
 public class SpringConfig {
   
+  private DataSource datasource;
+
+  @Autowired
+  public SpringConfig(DataSource datasource) {
+    this.datasource = datasource;
+  }
+
   @Bean
   public MemberService memberService() {
     return new MemberService(memberRepository());
@@ -17,7 +28,8 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+//    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(datasource);
   }
-  
+
 }
